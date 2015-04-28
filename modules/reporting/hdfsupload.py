156db7655a23031d562b225dabfce2ff552d3ed6
cjmateos <mateos83@gmail.com>
2015-04-28 12:57:14 +0200

    Update hdfsupload.py
    Check destination folder in hadoop. If not exists, then it's created

--- modules/reporting/hdfsupload.py
+++ modules/reporting/hdfsupload.py
@@ -1,14 +1,16 @@
-import os 
+import os
 
-from lib.cuckoo.common.exceptions import CuckooReportError 
-from lib.cuckoo.common.abstracts import Report 
+from lib.cuckoo.common.exceptions import CuckooReportError
+from lib.cuckoo.common.abstracts import Report
 
-class HdfsUpload(Report): 
-    """Saves analysis results in HDFS node when the proccess has been finished""" 
+class HdfsUpload(Report):
+    """Saves analysis results in HDFS node when the proccess has been finished"""
 
-    def run(self, results): 
-        try: 
-            path = str(self.analysis_path) 
-            os.system("hdfs dfs -put %s hdfs://hadoopnamenode.redborder.cluster:8020" % path) 
-        except (UnicodeError, TypeError, IOError) as e: 
-            raise CuckooReportError("Failed to generate HDFSUpload report: %s" % e) 
+    def run(self, results):
+        try:
+            if os.system("hdfs dfs -test -e hdfs://hadoopnamenode.redborder.cluster:8020/user/cuckoo/"):
+                    os.system("hdfs dfs -mkdir hdfs://hadoopnamenode.redborder.cluster:8020/user/cuckoo")
+            path = str(self.analysis_path)
+            os.system("hdfs dfs -put %s hdfs://hadoopnamenode.redborder.cluster:8020/user/cuckoo/" % path)
+        except (UnicodeError, TypeError, IOError) as e:
+            raise CuckooReportError("Failed to generate HDFSUpload report: %s" % e)
