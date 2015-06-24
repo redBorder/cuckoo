205f8ffd58c860483cf7321b3ead1d68bccd2e4d
cjmateos <mateos83@gmail.com>
2015-06-24 18:25:18 +0200

    Update and rename hdfsupload.py to uploadhdfs.py

--- modules/reporting/uploadhdfs.py
+++ modules/reporting/uploadhdfs.py
@@ -3,7 +3,7 @@ import os
 from lib.cuckoo.common.exceptions import CuckooReportError
 from lib.cuckoo.common.abstracts import Report
 
-class HdfsUpload(Report):
+class UploadHdfs(Report):
     """Saves analysis results in HDFS node when the proccess has been finished"""
 
     def run(self, results):
